b349fcdec151489eb0a123c07ea1f2c160f5c15e
iStrokes <felipebruckmann@hotmail.com.br>
2020-08-05 23:50:08 -0300

    segundo dia3

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,25 @@
 import express from 'express'
+import db from './database/connection';
 const routes = express.Router()
 
 
 
-routes.get('/', (req, res) => {
-  console.log("hello WOrld") 
+routes.post('/classes', async (req, res) => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    cost,
+    schedule
+  } = req.body;
+
+  await db('users').insert({
+    name,
+    avatar,
+    whatsapp,
+    bio,
+  })
 })
 
 export default routes;
