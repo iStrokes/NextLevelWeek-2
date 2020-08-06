3140fc84b7529707da44f23890c984b278947a12
iStrokes <felipebruckmann@hotmail.com.br>
2020-08-05 21:45:28 -0300

    segundo dia

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,3 +1,12 @@
-import knew from 'knex'
+import knex from 'knex';
+import path from 'path';
 
-const db = knew()
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'database.sqlite')
+  },
+  useNullAsDefault: true
+});
+
+export default db;
